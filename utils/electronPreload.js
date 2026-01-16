3f76ba257cd1f944e1839ec239bd49d86faf0165
userdie55 <akulenko.leonid@protonmail.com>
2026-01-16 12:51:02 +0300

    Update electronPreload.js

--- utils/electronPreload.js
+++ utils/electronPreload.js
@@ -1,6 +1,7 @@
-const { contextBridge, ipcRenderer } = require("electron");
+import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("layoutAPI", {
   load: () => ipcRenderer.invoke("layout:load"),
   save: (layout) => ipcRenderer.invoke("layout:save", layout),
 });
+
